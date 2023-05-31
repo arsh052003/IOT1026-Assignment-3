8ca1077ce30e6f59e8dcfeb584ff3ea23d6b17fd
arsh052003 <122691385+arsh052003@users.noreply.github.com>
2023-05-31 17:23:46 +0000

    Implement correct weight and volume

--- Assignment/Pack.cs
+++ Assignment/Pack.cs
@@ -22,16 +22,63 @@ class Pack
     }
 }
 
-class InventoryItem
+// Come back to this once we learn about abstract classes.
+public class InventoryItem
 {
-    // Implement this class
+    private readonly float _volume;
+    private readonly float _weight;
+
+    public InventoryItem(float weight, float volume)
+    {
+        if (volume <= 0f || weight <= 0f)
+        {
+            throw new ArgumentOutOfRangeException($"An item can't have {volume} volume or {weight} weight");
+        }
+        _volume = volume;
+        _weight = weight;
+    }
+
+    // Getters
+    public float GetVolume()
+    {
+        return _volume;
+    }
+
+    public float GetWeight()
+    {
+        return _weight;
+    }
 }
 
 // Implement these classes - each inherits from InventoryItem
 // 1 line of code each - call base class constructor with appropriate arguments
-class Arrow : InventoryItem { }
-class Bow : InventoryItem { }
-class Rope : InventoryItem { }
-class Water : InventoryItem { }
-class Food : InventoryItem { }
-class Sword : InventoryItem { }
+class Arrow : InventoryItem
+{
+    public Arrow() : base(0.1f, 0.05f) { }
+}
+
+class Bow : InventoryItem
+{
+    public Bow() : base(1f, 4f) { }
+}
+
+class Rope : InventoryItem
+{
+    public Rope() : base(1f, 1.5f) { }
+}
+
+class Water : InventoryItem
+{
+    public Water() : base(2f, 3f) { }
+
+}
+class Food : InventoryItem
+{
+    public Food() : base(1f, 0.5f) { }
+
+}
+class Sword : InventoryItem
+{
+    public Sword() : base(5f, 3f) { }
+
+}
